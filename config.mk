85389af03505a23fe222b002b926f21d0a37d626
kaka555 <493321524@qq.com>
2020-09-26 11:13:00 +0800

    suit my stm32f103zet6 board(minimum system board)

--- config.mk
+++ config.mk
@@ -1,1 +1,9 @@
-
+CONFIG_NAME := stm32f103_config
+export CONFIG_NAME
+PROJECT := stm32f103
+export PROJECT
+FLAGS_AUXILIARY := -mcpu=cortex-m3 -mthumb
+export FLAGS_AUXILIARY
+DEF := -D STM32F10X_HD -D USE_STDPERIPH_DRIVER
+export DEF
+LIBPATH := -L /usr/lib/gcc/arm-none-eabi/4.9.3/armv7e-m/softfp
